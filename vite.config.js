21a923c3a26c4f920cdc2ee1027899bef327f026
DraconikCatastrophe <abenk223jr@gmail.com>
2025-06-13 16:14:46 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/website-personal1/', // ⬅️ ganti sesuai nama repo GitHub-mu
+  base: '/website-personal1/', // ← ini HARUS sesuai dengan nama repo kamu di GitHub
   plugins: [react()],
 })
